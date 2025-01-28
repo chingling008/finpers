cec63530b760040ade91257c113e6a45f5d97f52
chingling008 <130581550+chingling008@users.noreply.github.com>
2025-01-28 16:48:56 +0000

    Update on firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBHN6y4_1GPdC3BqSKItm8djOahV7sr6Qs",
   authDomain: "money-mangement-58tlyi.firebaseapp.com",
   projectId: "money-mangement-58tlyi",
-  storageBucket: "money-mangement-58tlyi.firebasestorage.app",
+  storageBucket: "money-mangement-58tlyi.firebasestorage.app",//Does not work cause I need to pay it
   messagingSenderId: "849757694569",
   appId: "1:849757694569:web:b1e85074a0795b0bf010f8"
 };
